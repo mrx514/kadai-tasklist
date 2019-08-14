3b5dde738b37f1dc2e2e57877b40554e51392145
masa takeuchi <mrx514@yahoo.co.jp>
2019-08-14 13:09:59 +0000

    task-list kadai2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_104735) do
+ActiveRecord::Schema.define(version: 2019_08_14_112504) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
